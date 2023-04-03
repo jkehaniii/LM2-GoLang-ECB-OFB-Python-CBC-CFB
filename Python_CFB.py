3989a1be261b9d4282f3f543dd057dca25c68eb6
James Kehan <88910276+jkehaniii@users.noreply.github.com>
2023-04-03 11:05:12 -0400

    Update Python_CFB.py

--- Python_CFB.py
+++ Python_CFB.py
@@ -1,15 +1,14 @@
 # Import AES from appropriate libraries
 from Crypto.Cipher import AES
 
-value = input("Enter a plaintext value: ")
-
 # Initial key and plaintext value
 key = b'0123456789abcdef'
-plaintext = value
+plaintext = b'Hello, world!'
 
 # Creates a cipher using CFB thanks to AES library
 cipher = AES.new(key, AES.MODE_CFB)
 ciphertext = cipher.encrypt(plaintext)
 iv = cipher.iv
 
+print('Plaintext: {}'.format(plaintext))
 print(f"CFB encrypted: {ciphertext}")
