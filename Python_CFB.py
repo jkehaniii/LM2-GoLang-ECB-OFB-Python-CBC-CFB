ad2e9cd0176aec6e6e072d653703746c4bd1ed24
James Kehan <88910276+jkehaniii@users.noreply.github.com>
2023-04-04 09:47:15 -0400

    Update Python_CFB.py

--- Python_CFB.py
+++ Python_CFB.py
@@ -3,7 +3,7 @@ from Crypto.Cipher import AES
 
 # Initial key and plaintext value
 key = b'0123456789abcdef'
-plaintext = b'Hello, world!'
+plaintext = b'This is our plaintext message for IST 402'
 
 # Creates a cipher using CFB thanks to AES library
 cipher = AES.new(key, AES.MODE_CFB)
